a7e1f5a250ded1c614fb8870fdea2608541b56a3
andyengit <anderson.armeya@gmail.com>
2026-04-07 00:33:15 +0000

    Initial commit for the Currency Rate Provider module for BCV, including manifest, models, translations, and documentation files.

---
+++ res_currency_rate_provider_BCV/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "Currency rate provider: BCV",
+    "icon": "/poweredbyandy_saas/static/description/icon.png",
+    "summary": """Currency rate provider for BCV (Banco Central de Venezuela)""",
+    "version": "18.0.1.1.2",
+    "category": "Financial Management/Configuration",
+    "author": "Anderson Armeya, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-venezuela",
+    "maintainers": ["andyengit"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["currency_rate_update"],
+    "data": [],
+}
